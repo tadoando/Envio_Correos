9378d53914b3182ababb4e7bab90458ea0772e65
tadoando <andopolio@gmail.com>
2024-02-29 15:37:03 -0500

    Funcional

--- app_correo.py
+++ app_correo.py
@@ -1,11 +1,28 @@
 #%%
 import win32com.client
-#%%
-outlook = win32com.client.Dispatch("Outlook.Application")
-#%%
-def envio_corre(Destinatarios, titulo, cuerpo):
-    mail = outlook.CreateItem(0)
-    mail.To = Destinatarios
-    mail.Subject = titulo
-    mail.Body = cuerpo
-    mail.Send()
+from PARAMETROS import DESTINATARIOS, TITULO, CUERPO_CORREO, ARCHIVO
+
+def enviar_correo(destinatarioS, asunto, cuerpo, adjunto):
+    outlook = win32com.client.Dispatch("Outlook.Application")
+    for destinatario in destinatarioS:
+        try:
+            correo = outlook.CreateItem(0)
+            correo.To = destinatario
+            correo.Subject = asunto
+            correo.HTMLBody = cuerpo
+            
+            # Adjuntar archivo
+            adjunto_path = adjunto
+            correo.Attachments.Add(adjunto_path)
+            
+            # Enviar correo
+            correo.Send()
+            print("Correo enviado correctamente a", destinatario)
+        except Exception as e:
+            print(f"Error al enviar correo a {destinatario}: {e}")
+
+# Ejemplo de uso
+if __name__ == "__main__":
+    
+    enviar_correo(DESTINATARIOS, TITULO, CUERPO_CORREO, ARCHIVO)
+# %%
