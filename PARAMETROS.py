0ff83807381d3a27fdc41c995b1352f2378232fe
TadoAndo <andopolio@gmail.com>
2024-02-29 21:11:19 -0500

    email

--- PARAMETROS.py
+++ PARAMETROS.py
@@ -1,5 +1,5 @@
 #PARAMETROS
-DESTINATARIOS = ["manuel.amado@grocom.co"]
+DESTINATARIOS = ["manuel.amado@groupcos.com.co"]
 TITULO = "Test 2"
 CUERPO_CORREO = "Cuerpo del correo"
 ARCHIVO = "C://Users//manuel.amado//Documents//CUMPLIMIENTO_REPORTING_v.4.xlsm"
